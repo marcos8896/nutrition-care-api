9d9e744dfd984ea5d4051f4ce34b946537ae5166
marcos8896 <elyomarcos@gmail.com>
2018-03-04 13:11:54 -0600

    Added UserFactory.

---
+++ database/seeds/UserFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserFactory extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
