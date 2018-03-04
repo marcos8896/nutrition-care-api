47edd2de88ff8110a2f1ed6ef56d87905b0f9b98
marcos8896 <elyomarcos@gmail.com>
2018-03-04 13:11:30 -0600

    Some little test changes to the routes file.

--- app/Http/Resources/FoodCollection.php
+++ app/Http/Resources/FoodCollection.php
@@ -14,6 +14,14 @@ class FoodCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+
+        return [
+          'data' => $this->collection,
+          'links' => [
+              'self' => 'link-value',
+          ],
+      ];
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Food;
+use App\User;
 use App\Http\Resources\FoodResource;
+use App\Http\Resources\UserResource;
 
 
 Route::get('/', function () {
@@ -9,10 +11,18 @@ Route::get('/', function () {
 });
 
 
-Route::get('/food', function () {
-  return FoodResource::collection(Food::all());
+Route::get('api/food/all', function () {
+  return FoodResource::collection(Food::paginate());
 });
 
-Route::get('/{id}', function ($id) {
+Route::get('api/food/{id}', function ($id) {
     return new FoodResource(Food::find($id));
+});
+
+Route::get('api/user/all', function () {
+  return UserResource::collection(User::paginate());
+});
+
+Route::get('api/user/{id}', function ($id) {
+    return new UserResource(User::find($id));
 });
