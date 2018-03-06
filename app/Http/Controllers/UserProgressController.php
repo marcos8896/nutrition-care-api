f3ee2b566977f1986811e70ef5dc50e490dc0941
marcos8896 <elyomarcos@gmail.com>
2018-03-06 15:30:02 -0600

    Added main structure for the UserProgress' controller.

---
+++ app/Http/Controllers/UserProgressController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UserProgress;
+use Illuminate\Http\Request;
+
+class UserProgressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UserProgress  $userprogress
+     * @return \Illuminate\Http\Response
+     * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+     */
+    public function show(UserProgress $userprogress)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UserProgress  $userprogress
+     * @return \Illuminate\Http\Response
+     * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+     */
+    public function update(Request $request, UserProgress $userprogress)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UserProgress  $userprogress
+     * @return \Illuminate\Http\Response
+     * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+     */
+    public function destroy(UserProgress $userprogress)
+    {
+        //
+    }
+}
