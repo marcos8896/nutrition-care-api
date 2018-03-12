4b4424f0698ca67439be1b15ea9e8da86804e25b
marcos8896 <elyomarcos@gmail.com>
2018-03-12 16:28:26 -0600

    Added main structure for BodyArea's controller.

---
+++ app/Http/Controllers/BodyAreaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BodyArea;
+use Illuminate\Http\Request;
+use App\Http\Resources\BodyAreaResource;
+
+
+class BodyAreaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return BodyAreaResource::collection(BodyArea::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BodyArea  $bodyArea
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BodyArea $bodyarea)
+    {
+        return new BodyAreaResource( BodyArea::find($bodyarea->id) );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BodyArea  $bodyArea
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BodyArea $bodyArea)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BodyArea  $bodyArea
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BodyArea $bodyArea)
+    {
+        //
+    }
+}
