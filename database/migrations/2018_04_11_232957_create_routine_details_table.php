5cd08750ef5c5e247a700d65fc929d4b430fcb43
Brandon Villa <bornofos@gmail.com>
2018-04-11 18:30:58 -0500

    Routine migration initiated

---
+++ database/migrations/2018_04_11_232957_create_routine_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoutineDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routine_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routine_details');
+    }
+}
