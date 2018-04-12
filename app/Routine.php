c476e459a705e6717cee8f0cf7fca4191c7bdfca
Brandon Villa <bornofos@gmail.com>
2018-04-11 19:59:05 -0500

    routineDetails relationship of hasMany added

--- app/Routine.php
+++ app/Routine.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\RoutineDetail;
 
 class Routine extends Model
 {
-    //
+    protected $fillable = [
+        'description', 'state'
+    ];
+
+    /**
+     * Eloquent's definition of 'hasMany' relationship with a RoutineDetail instance.
+     *
+     * @param  {*}
+    */
+    public function routineDetails()
+    {
+      return $this->hasMany(RoutineDetail::class);
+    }
 }
