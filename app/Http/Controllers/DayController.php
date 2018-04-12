9e78e95246ede6a7eb106fed36dae5c0581eb27e
Brandon Villa <bornofos@gmail.com>
2018-04-11 22:28:52 -0500

    index method to return all days in table

--- app/Http/Controllers/DayController.php
+++ app/Http/Controllers/DayController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Day;
+use App\Http\Resources\DayResource;
+
 use Illuminate\Http\Request;
 
 class DayController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return DayResource::collection(Day::paginate(10));
+    }
 }
