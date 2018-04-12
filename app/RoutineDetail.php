baefba1788b35b2c65bb48009ee15e42369c8650
Brandon Villa <bornofos@gmail.com>
2018-04-11 20:08:03 -0500

    routine relationship belongsTo added

--- app/RoutineDetail.php
+++ app/RoutineDetail.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Routine;
 
 class RoutineDetail extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'day_id', 'series', 'reps', 'description'
+    ];
+    
+    /**
+     * Eloquent's definition of 'belongsTo' relationship with an Routine instance.
+     *
+     * @param  {*}
+    */
+    public function routine() {
+        return $this->belongsTo(Routine::class);
+    }
 }
