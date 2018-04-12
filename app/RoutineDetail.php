656a7cde2675f5ec3da07719402ea35e25e64e27
Brandon Villa <bornofos@gmail.com>
2018-04-11 21:57:15 -0500

    exercise and day relationships added

--- app/RoutineDetail.php
+++ app/RoutineDetail.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Routine;
+use App\Exercise;
+use App\Day;
 
 class RoutineDetail extends Model
 {
@@ -12,11 +14,30 @@ class RoutineDetail extends Model
     ];
     
     /**
-     * Eloquent's definition of 'belongsTo' relationship with an Routine instance.
+     * Eloquent's definition of 'belongsTo' relationship with a Routine instance.
      *
      * @param  {*}
     */
     public function routine() {
         return $this->belongsTo(Routine::class);
     }
+
+    /**
+     * Eloquent's definition of 'belongsTo' relationship with an Exercise instance.
+     *
+     * @param  {*}
+    */
+    public function exercise() {
+        return $this->belongsTo(Exercise::class);
+    }
+
+    /**
+     * Eloquent's definition of 'belongsTo' relationship with a Day instance.
+     *
+     * @param  {*}
+    */
+    public function day() {
+        return $this->belongsTo(Day::class);
+    }
+
 }
