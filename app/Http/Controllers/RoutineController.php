f1d4e6459f8e1251264452a1303e1dbeff3ec542
Brandon Villa <bornofos@gmail.com>
2018-04-11 23:23:42 -0500

    index method created to return all routines with relationships

--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Routine;
+use App\Http\Resources\RoutineResource;
+
 use Illuminate\Http\Request;
 
 class RoutineController extends Controller
 {
-    //
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      return RoutineResource::collection(Routine::with('routineDetails', 'user')->get());
+    }
 }
