b00271dfa76ec49a3aacc95ffb16d73ad486f6ef
Brandon Villa <bornofos@gmail.com>
2018-04-11 22:29:18 -0500

    Day resource-collection modified

--- app/Http/Resources/DayCollection.php
+++ app/Http/Resources/DayCollection.php
@@ -14,6 +14,11 @@ class DayCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => $this->collection,
+            'links' => [
+                'self' => 'link-value'
+            ],
+        ];
     }
 }
