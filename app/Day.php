80a0641310c6dab8cfb0fd5d419b21d11445b418
Brandon Villa <bornofos@gmail.com>
2018-04-11 21:56:53 -0500

    routineDetails relationship added

--- app/Day.php
+++ app/Day.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\RoutineDetail;
 
 class Day extends Model
 {
-    //
+    protected $fillable = [
+        'description'
+    ];
+
+    /**
+     * Eloquent's definition of 'hasMany' relationship with a RoutineDetail instance.
+     *
+     * @param  {*}
+    */
+    public function routineDetails() {
+        return $this->hasMany(RoutineDetails::class);
+    }
 }
