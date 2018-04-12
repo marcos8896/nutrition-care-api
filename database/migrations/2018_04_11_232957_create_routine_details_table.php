68e5ae755df5d95da2fe1fe0f8003e4891d62045
Brandon Villa <bornofos@gmail.com>
2018-04-11 19:59:43 -0500

    RoutineDetail fileds added

--- database/migrations/2018_04_11_232957_create_routine_details_table.php
+++ database/migrations/2018_04_11_232957_create_routine_details_table.php
@@ -12,9 +12,22 @@ class CreateRoutineDetailsTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   
         Schema::create('routine_details', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('routine_id')->unsigned()->index();
+            $table->foreign('routine_id')->references('id')->on('routines');
+
+            $table->integer('exercise_id')->unsigned()->index();
+            $table->foreign('exercise_id')->references('id')->on('exercises');
+            
+            $table->integer('day_id')->unsigned()->index();
+            $table->foreign('day_id')->references('id')->on('days');
+
+            $table->text('series');
+            $table->text('reps');
+            $table->text('description');
+
+            $table->primary(['routine_id', 'exercise_id', 'day_id']);
             $table->timestamps();
         });
     }
