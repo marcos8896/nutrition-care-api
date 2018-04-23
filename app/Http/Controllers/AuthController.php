ca959362e4d0ea2a24e26c7d00572b794dad9352
Brandon Villa <bornofos@gmail.com>
2018-04-23 15:27:33 -0500

    Nutrition care logo added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,11 +34,11 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
-
+        
         if ($token = $this->guard()->attempt($credentials)) {
             return $this->respondWithToken($token);
         }
-
+        
         return response()->json(['error' => 'Unauthorized'], 401);
     }
 
